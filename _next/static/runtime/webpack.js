6e10d7b62c1f74066e67db5eb2d440babb35def6
Srikanth Sharma <snow.650@gmail.com>
2019-12-22 20:49:37 +0530

    Build

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "68cada1b68dabe165b35";
+/******/ 	var hotCurrentHash = "bc54f263cb68da0dda70";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
